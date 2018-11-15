f797de0bbe7e14b8a39cd8aa702ef2658bb30fe7
Erik Zettersten <erik@zettersten.com>
2018-11-14 20:57:38 -0500

    need node sasss

--- gulpfile.js
+++ gulpfile.js
@@ -29,3 +29,5 @@ gulp.task('panini:watch', function() {
 gulp.task('sass:watch', function () {
     gulp.watch('./sass/**/*.scss', ['sass']);
 });
+
+gulp.task('default', ['panini:watch', 'sass:watch']);
