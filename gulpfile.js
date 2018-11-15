31d2e586ffd262b8dce5721c6e268f1371efe497
Erik Zettersten <erik@zettersten.com>
2018-11-14 20:55:49 -0500

    adds deps

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp');
+var panini = require('panini');
+var sass = require('gulp-sass');
+
+sass.compiler = require('node-sass');
+
+gulp.task('sass', function () {
+    return gulp.src('./sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css'));
+});
+
+gulp.task('panini', function () {
+    gulp.src('pages/**/*.html')
+        .pipe(panini({
+            root: 'pages/',
+            layouts: 'layouts/',
+            partials: 'partials/',
+            helpers: 'helpers/',
+            data: 'data/'
+        }))
+        .pipe(gulp.dest('build'));
+});
+
+gulp.task('panini:watch', function() {
+    gulp.watch(['./src/{layouts,partials,helpers,data}/**/*'], [panini.refresh]);
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./sass/**/*.scss', ['sass']);
+});
